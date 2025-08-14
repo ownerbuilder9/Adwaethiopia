d3973897bd75206baa479602d4dd5dc2ab65dd80
ownerbuilder9 <ownerbuilder9@gmail.com>
2025-08-14 23:16:42 +0300

    Create projects.js

---
+++ projects.js
@@ -0,0 +1,14 @@
+const projects = [
+    {
+        "title": "በፍቅር ለተጎዱ - New Ai music From \"Adewa Ethiopia Channel\"",
+        "url": "https://www.youtube.com/embed/ovxv82-iwLc"
+    },
+    {
+        "title": "ኤፍሬም ታምሩ | እንቅልፍ እማይዎስደኝ-Ephrem Tamiru- Cover Ai ከቨር-Enkilf Emywosdegn | New ethiopian concert",
+        "url": "https://www.youtube.com/embed/gEOOIwsZdOA"
+    },
+    {
+        "title": "Tizita 2-ትዝታ 2 - Ethiopian Ai Music by Adewa Ethiopia Channel 2025 Oficial",
+        "url": "https://www.youtube.com/embed/OmwLM0bbluI"
+    }
+];
